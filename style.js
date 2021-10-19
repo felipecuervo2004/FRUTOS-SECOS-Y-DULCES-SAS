2d4f2bc30454e29643f7a116fe9bdd0071b4c4c5
felipecuervo2004 <82343812+felipecuervo2004@users.noreply.github.com>
2021-10-19 18:08:04 -0500

    first commit

---
+++ style.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu-btn").addEventListener("click", () =>{
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards' , {delay: 500});
+ScrollReveal().reveal('.banner' , {delay: 500});
+ScrollReveal().reveal('.banner-dos' , {delay: 500});
+ScrollReveal().reveal('.footer-link' , {delay: 500});
+ScrollReveal().reveal('.footer' , {delay: 500});
